610ae9496189d49bf74de4946ffe42836be1a568
paptreek <paptreek@hotmail.com>
2026-06-05 11:29:00 -0500

    acceleration and rotation
    - player can accelerate forward, automatically slows down after releasing input
    - player can rotate (turn) left and right to change direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,30 +3,49 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float _moveSpeed = 500;
+    private float _turnSpeed = 250;
     private Rigidbody2D _rb;
     private InputAction _moveAction;
+    private InputAction _turnAction;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _moveAction = InputSystem.actions.FindAction("Move");
+        _turnAction = InputSystem.actions.FindAction("Turn");
     }
 
     private void FixedUpdate()
     {
-        CheckForMovement();
+        Accelerate();
+        Turn();
     }
 
-    private void CheckForMovement()
+    private void Accelerate()
     {
-        if (_moveAction.IsPressed())
-        {
-            _rb.linearVelocityY = 1; // need to find a way for movement to be gradually increasing/decreasing values. not just on/off
-        }
-        else
-        {
-            _rb.linearVelocityY = 0;
-        }
+        float moveValue = _moveAction.ReadValue<float>();
+        float moveSpeed = _moveSpeed * Time.fixedDeltaTime;
+        float maxVelocity = 2.5f;
 
+        if (_rb.linearVelocityY <= maxVelocity)
+        {
+            _rb.AddRelativeForceY(moveValue * moveSpeed);
+        }
+    }
+
+    private void Turn()
+    {
+        Vector2 inputValue = _turnAction.ReadValue<Vector2>();
+        float turnSpeed = _turnSpeed * Time.fixedDeltaTime;
+
+        if (inputValue.x == -1)
+        {
+            transform.eulerAngles += new Vector3(0, 0, turnSpeed);
+        }
+        else if (inputValue.x == 1)
+        {
+            transform.eulerAngles += new Vector3(0, 0, -turnSpeed);
+        }
     }
 }
