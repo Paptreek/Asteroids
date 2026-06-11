784c1c64d1068b96aa7d48d8902335c92044dd18
paptreek <paptreek@hotmail.com>
2026-06-11 17:20:33 -0500

    score keeping added
    - player gets points for each asteroid killed
    - larger asteroids give less, smaller give more
    - bonus points added each round based on clear speed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,21 +6,25 @@ public class GameManager : MonoBehaviour
     [SerializeField] private AsteroidManager _asteroidManager;
     [SerializeField] private AsteroidSpawner _asteroidSpawner;
 
+    private bool _playerHasWon;
     private int _round = 1;
     private int _score;
-    private bool _playerHasWon;
+    private int _bonusScore;
+    private float _roundTimer = 120.0f;
 
     private void Start()
     {
-        _asteroidSpawner.SpawnNewRound(1, Asteroid.Size.Small, _asteroidManager.Asteroids);
+        _asteroidSpawner.SpawnNewRound(4, Asteroid.Size.Large, _asteroidManager.Asteroids);
     }
 
     private void Update()
     {
+        _roundTimer -= Time.deltaTime;
+
         CheckToStartNewRound();
         CheckForGameOver();
 
-        Debug.Log($"Score: {GetCurrentScore()}");
+        Debug.Log($"Score: {GetScore()}");
     }
 
     private void CheckToStartNewRound()
@@ -31,9 +35,7 @@ public class GameManager : MonoBehaviour
         {
             if (_round < maxRounds)
             {
-                _round++;
-                Debug.Log($"Round complete. Starting round {_round}!");
-                _player.ResetPosition();
+                ResetRound();
 
                 if (_player != null)
                 {
@@ -60,12 +62,33 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private int GetCurrentScore()
+    private int GetScore()
     {
         int pointsForLarge = _asteroidManager.LargeAsteroidsDestroyed * 50;
         int pointsForMedium = _asteroidManager.MediumAsteroidsDestroyed * 100;
         int pointsForSmall = _asteroidManager.SmallAsteroidsDestroyed * 250;
+        int baseScore = pointsForLarge + pointsForMedium + pointsForSmall;
 
-        return _score = pointsForLarge + pointsForMedium + pointsForSmall;
+        return _score = _bonusScore + baseScore;
+    }
+
+    private void ResetRound()
+    {
+        AddBonusScore();
+
+        _round++;
+        Debug.Log($"Round complete. Starting round {_round}!");
+
+        _roundTimer = 120.0f;
+        _player.ResetPosition();
+    }
+
+    private void AddBonusScore()
+    {
+        int pointsToAdd = Mathf.RoundToInt(_roundTimer) * 25;
+
+        Debug.Log($"Time Left: {_roundTimer}, Points Added: {pointsToAdd}");
+
+        _bonusScore += pointsToAdd;
     }
 }
